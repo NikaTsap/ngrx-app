a455194d22c28012a8a96738abcf943e2930d3a7
Veronika Smorodska <veronika.smorodska@gmail.com>
2022-02-21 12:36:01 +0200

    count.reducer added.

---
+++ src/app/reducers/count/count.reducer.ts
@@ -0,0 +1,12 @@
+export interface CountState {
+    count: number;
+    updatedAt: number;
+}
+
+const initialState: CountState = {
+    count: 0,
+    updatedAt: Date.now()
+};
+export const countReducer = (state = initialState) => {
+    return state;
+};
