ffaa0b849eb1844834e7d77e10b534f5b03a0585
Veronika Smorodska <veronika.smorodska@gmail.com>
2022-02-21 13:31:47 +0200

    disabling buttons if count is at zero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { CountClearAction, CountDecreaseAction, CountIncreaseAction } from './reducers/count/count.actions';
 import { CountState } from './reducers/count/count.reducer';
 import { selectCount, selectUpdatedAt } from './reducers/count/count.selectors';
@@ -13,6 +13,8 @@ import { selectCount, selectUpdatedAt } from './reducers/count/count.selectors';
 export class AppComponent {
   public count$: Observable<number> = this.store$.pipe(select(selectCount));
   public updatedAt$: Observable<number> = this.store$.pipe(select(selectUpdatedAt));
+
+  public isDecreaseDisabled$: Observable<boolean> = this.count$.pipe(map(count => count <= 0));
 
   constructor(private store$: Store<CountState>) {}
 
