47ea64b2fb31c8148f34397e519849e0c170695d
Veronika Smorodska <veronika.smorodska@gmail.com>
2022-02-21 13:22:51 +0200

    count actions dispatched in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { CountClearAction, CountDecreaseAction, CountIncreaseAction } from './reducers/count/count.actions';
 import { CountState } from './reducers/count/count.reducer';
 import { selectCount, selectUpdatedAt } from './reducers/count/count.selectors';
 
@@ -16,14 +17,14 @@ export class AppComponent {
   constructor(private store$: Store<CountState>) {}
 
   increase() {
-
+    this.store$.dispatch(new CountIncreaseAction());
   }
 
   decrease() {
-
+    this.store$.dispatch(new CountDecreaseAction());
   }
 
   clear() {
-
+    this.store$.dispatch(new CountClearAction());
   }
 }
