45856e7073400b07e105c0635a9ade4c9a408744
Veronika Smorodska <veronika.smorodska@gmail.com>
2022-02-21 13:19:16 +0200

    count actions added

--- src/app/reducers/count/count.reducer.ts
+++ src/app/reducers/count/count.reducer.ts
@@ -1,3 +1,6 @@
+import { Action } from "@ngrx/store";
+import { CountActions, countActionsType } from "./count.actions";
+
 export const countNode = "count";
 
 export interface CountState {
@@ -9,6 +12,26 @@ const initialState: CountState = {
     count: 0,
     updatedAt: Date.now()
 };
-export const countReducer = (state = initialState) => {
-    return state;
+export const countReducer = (state = initialState, action: Action) => {
+    switch (action.type) {
+        case countActionsType.increase:
+            return {
+                ...state,
+                // Not ++ because ++ changes the old state, which should not hapen in pure functions.
+                count: state.count + 1,
+            };
+        case countActionsType.decrease:
+            return {
+                ...state,
+                // Not -- because -- changes the old state, which should not hapen in pure functions.
+                count: state.count - 1,
+            };
+        case countActionsType.clear:
+            return {
+                ...state,
+                count: 0,
+            };
+        default:
+            return state;
+    }
 };
